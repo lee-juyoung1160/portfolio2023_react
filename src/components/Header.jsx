88a5fb9d72269fa765256032eb7afee3ad1a7d66
julia <noocgs@gmail.com>
2023-10-28 14:51:05 +0900

    헤더영역 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
+
+const headerNav = [
+    {
+        title: "Home",
+        url: "#visual"
+    },
+    {
+        title: "About",
+        url: "#about"
+    },
+    {
+        title: "Work",
+        url: "#work"
+    },
+    {
+        title: "Skill",
+        url: "#skill"
+    }
+];
 
 const Header = () => {
-    return <div>Header</div>;
-};
 
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    };
+
+    return(
+    <header id="header">
+    <h1 className="header__logo"><a href="/"><span>julia</span> Portfolio</a></h1>
+        <nav className={`header__nav ${show ? "show" : ""}`} aria-label="메인 메뉴">
+            <ul>
+            {headerNav.map((nav, key) => (
+                <li key={key}>
+                    <a href={nav.url}>{nav.title}</a>
+                </li>
+            ))}
+            </ul>
+        </nav>
+        <button type="button" className="header__nav__mo" onClick={toggleMenu}>
+            <span></span>
+        </button>
+    </header>
+
+    );
+};
 export default Header;
