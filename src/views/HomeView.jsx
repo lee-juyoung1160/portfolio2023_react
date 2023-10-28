823770a22c5458bc18b025d3bc3a552fd914ea7f
julia <noocgs@gmail.com>
2023-10-28 13:56:41 +0900

    컴포넌트 완료

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Visual from "../components/Visual";
+import About from "../components/About";
+import Work from "../components/Work";
+import Skill from "../components/Skill";
+import Footer from "../components/Footer";
+import Skip from "../components/Skip";
+
+
+const HomeView = () => {
+    return (
+    <>
+        <Skip />
+        <Header />
+        <Main>
+            <Visual />
+            <About />
+            <Skill />
+            <Work />
+        </Main>
+        <Footer />
+    </>
+    );
+};
+
+export default HomeView;
